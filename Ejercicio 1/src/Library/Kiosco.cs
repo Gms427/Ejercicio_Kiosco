d7dfcc724fcde7f56e26c190b431787252549f0b
gms427 <gonzalo427@hotmail.com>
2020-09-01 19:46:55 -0300

    Añado el ejercicio 1 con los comentarios

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -4,12 +4,6 @@ namespace Expert_SRP
 {
     public class Kiosco 
     {
-        
-        /* 
-        Este método podría estar también en la clase Alfajor como un método de instancia, ya que la clase alfajor tiene acceso a su precio y se podría 
-        ahorrar el primer parámetro, pero para esto se debería cumplir que el método de convertir moneda este en otra clase, y que dicha clase sea accesible
-        desde Alfajor
-        */
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
