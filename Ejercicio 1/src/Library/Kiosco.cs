02edf484797d43a478cc1099cd2fefa2f587816c
gms427 <gonzalo427@hotmail.com>
2020-09-01 19:52:10 -0300

    Añado otro comentario

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -7,7 +7,7 @@ namespace Expert_SRP
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa;
+            return pesos >= a.PrecioDulce + a.PrecioMasa; // El cálculo del precio total del alfajor debería estar en la clsae alfajor, ya que tiene la información necesaria para hacerlo
         }
 
         // Este método podría ir en otra clase que se encargue de hacer conversiones entre monedas, no tiene por qué ser responsabilidad de la clase Kiosco
