45aa414ea1b9d30ff304fe0b54cc2f77fb1d7f70
gms427 <gonzalo427@hotmail.com>
2020-09-01 19:54:17 -0300

    Añado aclaración de qué no se está cumpliendo en cada comentario

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -7,10 +7,10 @@ namespace Expert_SRP
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa; // El cálculo del precio total del alfajor debería estar en la clsae alfajor, ya que tiene la información necesaria para hacerlo
+            return pesos >= a.PrecioDulce + a.PrecioMasa; // El cálculo del precio total del alfajor debería estar en la clsae alfajor, ya que tiene la información necesaria para hacerlo, no cumple con EXPERT.
         }
 
-        // Este método podría ir en otra clase que se encargue de hacer conversiones entre monedas, no tiene por qué ser responsabilidad de la clase Kiosco
+        // Este método podría ir en otra clase que se encargue de hacer conversiones entre monedas, no tiene por qué ser responsabilidad de la clase Kiosco, no cumple con SRP.
         private Double ConvertirAPesos(Double dinero, String moneda)
         {
             if (moneda.Equals("U$S")) 
