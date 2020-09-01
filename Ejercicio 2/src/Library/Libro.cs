357e980978906100ca034dd303e394feb79289ab
gms427 <gonzalo427@hotmail.com>
2020-09-01 20:27:39 -0300

    Añado los comentarios del ejercicio 2

---
+++ Ejercicio 2/src/Library/Libro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SRP
+{
+    /*
+        En este caso creo que no hay nada que incumpla SRP ni EXPERT,
+        ya que el libro solo tiene una única responsabilidad que es 
+        almacenarse y conoce los datos necesarios para hacerlo.
+    */
+    public class Libro
+    {
+
+        public string Titulo { get ; }
+        public string Autor { get ; }
+        public string Codigo { get ;  }
+        public string SectorBiblioteca { get ; set; }
+        public string EstanteBiblioteca { get ; set; }
+
+        public Libro(String titulo, String autor, String codigo)
+        {
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.Codigo = codigo;
+        }
+
+        public void AlmacenarLibro(String sector, String estante)
+        {
+            this.SectorBiblioteca = sector;
+            this.EstanteBiblioteca = estante;
+        }
+
+    }
+}
