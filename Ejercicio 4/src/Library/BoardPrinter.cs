e212416be5551fb5f56a650d358aae0b2c1786df
gms427 <gonzalo427@hotmail.com>
2020-09-08 19:50:28 -0300

    Corrección de un bug al imprimir

--- Ejercicio 4/src/Library/BoardPrinter.cs
+++ Ejercicio 4/src/Library/BoardPrinter.cs
@@ -12,12 +12,13 @@ namespace PII_Game_Of_Life
         /// </summary>
         public static void PrintBoard(Board board)
         {
-            bool[,] b = board.BoardData; //variable que representa el tablero
+            bool[,] b; //variable que representa el tablero
             int width = board.Width; //variabe que representa el ancho del tablero
             int height = board.Heigth; //variabe que representa altura del tablero
 
             while(true)
-            {
+            {   
+                b = board.BoardData;
                 Console.Clear();
                 StringBuilder s = new StringBuilder();
                 for (int y = 0; y < height; y++)
